ce5e7229f6fecd2fc4f510a38ec603cc9bb584ed
mrtanonur <onurtan@gmail.com>
2025-09-07 18:28:21 +0300

    feat(service): add firebase services

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,124 @@
+import 'package:either_dart/either.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+
+class AuthService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<bool> authCheck() async {
+    final user = _firebaseAuth.currentUser;
+    if (user != null) {
+      if (user.emailVerified) {
+        return true;
+      }
+      return false;
+    }
+    return false;
+  }
+
+  Future<Either<String, UserCredential>> signUp(
+    String email,
+    String password,
+  ) async {
+    try {
+      UserCredential userCredential = await _firebaseAuth
+          .createUserWithEmailAndPassword(email: email, password: password);
+      await _firebaseAuth.currentUser?.sendEmailVerification();
+
+      return Right(userCredential);
+    } on FirebaseAuthException catch (exception) {
+      print(exception);
+      return Left(exception.message!);
+    }
+  }
+
+  Future<Either<String, void>> sendVerificationLink() async {
+    try {
+      await _firebaseAuth.currentUser?.sendEmailVerification();
+      return Right(null);
+    } on FirebaseAuthException catch (exception) {
+      return Left(exception.message!);
+    }
+  }
+
+  Future<Either<String, User>> signIn(String email, String password) async {
+    try {
+      final UserCredential userCredential = await _firebaseAuth
+          .signInWithEmailAndPassword(email: email, password: password);
+      print(userCredential.user);
+      return Right(userCredential.user!);
+    } on FirebaseAuthException catch (exception) {
+      print(exception.message);
+      return Left(exception.message!);
+    }
+  }
+
+  //google sign in
+  Future<Either<String, void>> googleSignIn() async {
+    try {
+      final GoogleSignIn googleSignIn = GoogleSignIn.instance;
+      await googleSignIn.initialize();
+
+      print(googleSignIn);
+
+      final GoogleSignInAccount? googleUser = await googleSignIn
+          .attemptLightweightAuthentication();
+      print(googleUser);
+
+      googleUser ?? await googleSignIn.authenticate();
+
+      final GoogleSignInAuthentication googleAuth = googleUser!.authentication;
+
+      final userCredential = GoogleAuthProvider.credential(
+        idToken: googleAuth.idToken,
+      );
+      await _firebaseAuth.signInWithCredential(userCredential);
+
+      return const Right(null);
+    } on FirebaseAuthException catch (exception) {
+      return Left(exception.message!);
+    }
+  }
+
+  // apple sign in
+  Future<Either<String, User?>> appleSignIn() async {
+    try {
+      final appleCredential = await SignInWithApple.getAppleIDCredential(
+        scopes: [
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+      );
+      final oAuthCredential = OAuthProvider("apple.com").credential(
+        idToken: appleCredential.identityToken,
+        accessToken: appleCredential.authorizationCode,
+      );
+      final userCredential = await _firebaseAuth.signInWithCredential(
+        oAuthCredential,
+      );
+      return Right(userCredential.user);
+    } on FirebaseAuthException catch (exception) {
+      print(exception.message);
+      return Left(exception.message!);
+    }
+  }
+
+  Future<Either<String, void>> sendPasswordResetLink(String email) async {
+    try {
+      await _firebaseAuth.sendPasswordResetEmail(email: email);
+      return Right(null);
+    } on FirebaseAuthException catch (exception) {
+      return Left(exception.message!);
+    }
+  }
+
+  Future<Either<String, void>> signOut() async {
+    try {
+      await _firebaseAuth.signOut();
+      return const Right(null);
+    } on FirebaseAuthException catch (exception) {
+      return Left(exception.message!);
+    }
+  }
+}
---
+++ lib/services/firestore_service.dart
@@ -0,0 +1,106 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:either_dart/either.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:focusnotes/models/note_model.dart';
+import 'package:focusnotes/models/user_model.dart';
+
+class FirestoreService {
+  final FirebaseFirestore _firestoreService = FirebaseFirestore.instance;
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  final String _userCollectionName = "user";
+  final String _noteCollectionName = "note";
+
+  Future<Either<String, void>> storeUserData(UserModel userModel) async {
+    try {
+      String id = _firebaseAuth.currentUser!.uid;
+      await _firestoreService
+          .collection(_userCollectionName)
+          .doc(id)
+          .set(userModel.toJson());
+      return Right(null);
+    } on FirebaseException catch (exception) {
+      return Left(exception.message!);
+    }
+  }
+
+  Future<Either<String, UserModel>> getUserData() async {
+    try {
+      String id = _firebaseAuth.currentUser!.uid;
+      final response = await _firestoreService
+          .collection(_userCollectionName)
+          .doc(id)
+          .get();
+      print(response.data());
+      final UserModel userModel = UserModel.fromJson(response.data()!);
+      print(userModel.email);
+      return Right(userModel);
+    } on FirebaseException catch (exception) {
+      return Left(exception.message!);
+    }
+  }
+
+  Future<Either<String, void>> add(NoteModel noteModel) async {
+    try {
+      String id = _firebaseAuth.currentUser!.uid;
+      await _firestoreService
+          .collection(_userCollectionName)
+          .doc(id)
+          .collection(_noteCollectionName)
+          .doc(noteModel.id)
+          .set(noteModel.toJson());
+
+      return Right(null);
+    } on FirebaseException catch (exception) {
+      return Left(exception.message!);
+    }
+  }
+
+  Future<Either<String, void>> remove(String noteId) async {
+    try {
+      String id = _firebaseAuth.currentUser!.uid;
+      await _firestoreService
+          .collection(_userCollectionName)
+          .doc(id)
+          .collection(_noteCollectionName)
+          .doc(noteId)
+          .delete();
+      return Right(null);
+    } on FirebaseException catch (exception) {
+      return Left(exception.message!);
+    }
+  }
+
+  Future<Either<String, void>> update(NoteModel noteModel) async {
+    try {
+      String id = _firebaseAuth.currentUser!.uid;
+      await _firestoreService
+          .collection(_userCollectionName)
+          .doc(id)
+          .collection(_noteCollectionName)
+          .doc(noteModel.id)
+          .update(noteModel.toJson());
+      return Right(null);
+    } on FirebaseException catch (exception) {
+      return Left(exception.message!);
+    }
+  }
+
+  Future<Either<String, List<NoteModel?>>> getNotes() async {
+    try {
+      String id = _firebaseAuth.currentUser!.uid;
+      final snapshot = await _firestoreService
+          .collection(_userCollectionName)
+          .doc(id)
+          .collection(_noteCollectionName)
+          .get();
+
+      List<NoteModel> list = snapshot.docs
+          .map((doc) => NoteModel.fromJson(doc.data()))
+          .toList();
+      return Right(list);
+    } on FirebaseException catch (exception) {
+      return Left(exception.message!);
+    }
+  }
+}
